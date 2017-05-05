91629db741ecec5bc3eaa6d8927fb637a6789336
i471 <robertdgil@hotmail.com>
2017-05-04 23:01:55 -0700

    Added Classes
    Empty classes

---
+++ src/cs445/finalproject/CS445FinalProject.java
@@ -0,0 +1,23 @@
+/***************************************************************
+* file: CS445FinalProject.java
+* author: Robert Gil, Albert Gil, Ben , Nathaniel
+* class: CS 445 – Computer Graphics
+*
+* assignment: Quarter Project
+* date last modified: 5/4/17
+*
+* purpose: 
+* 
+* Current State of project: 
+*
+****************************************************************/
+package cs445.finalproject;
+
+
+public class CS445FinalProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
---
+++ src/cs445/finalproject/CheckPoint1.java
@@ -0,0 +1,19 @@
+/***************************************************************
+* file: CS445FinalProject.java
+* author: Robert Gil, Albert Gil, Ben , Nathaniel
+* class: CS 445 – Computer Graphics
+*
+* assignment: Quarter Project
+* date last modified: 5/4/17
+*
+* purpose: 
+* 
+* Current State of project: 
+*
+****************************************************************/
+package cs445.finalproject;
+
+
+public class CheckPoint1 {
+    
+}
